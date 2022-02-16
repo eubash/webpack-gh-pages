1966947628f11c4dee8d3c90b2793ade710564e9
Eubash <ssimplatina@gmial.com>
2022-02-16 16:51:33 +0100

    reset initial application

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,10 @@ function render() {
   document.querySelector("#app").innerHTML = window.location.href; //рендерим ссылку из поискового запроса
 }
 
-document.body.addEventListener("click", (ev) => {
-  if ((ev.target as HTMLElement).matches("a")) {
-    ev.preventDefault();
-    const url = (ev.target as HTMLElement).getAttribute("href");
-    if (window.history) {
-        window.history.pushState({}, url, url);
-    } else {
-      throw new Error("Sorry history api doesn't supported");
-    }
-    render();
-  }
+document.body.addEventListener("click", (ev) => { 
+ if ((ev.target as HTMLElement).matches("a")) {
+    render(); 
+ } 
 });
 
 render();
