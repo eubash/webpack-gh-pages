d727cec47c78cdc9f4814026d8a3ba7b9b9e6e20
eubash <eubash5657@gmail.com>
2025-02-11 17:28:49 +0000

    Deploying to gh-pages from @ eubash/webpack-gh-pages@12061c1fa5e3216f2139bc6ed2409c53f6a37cb2 🚀

--- bundle.js
+++ bundle.js
@@ -1,1 +1,1 @@
-!function(){function t(t){const e=window.location.href;document.querySelector("#app").textContent=t?e.includes("#")?e.replace(/#.*/,t):e+t:window.location.href}document.body.addEventListener("click",(e=>{e.target.matches("a")&&t(e.target.hash)})),t()}();
+!function(){function t(t){const e=window.location.href;document.querySelector("#app").textContent=t?e.includes("#")?e.replace(/#.*/,t):e+t:window.location.href}document.body.addEventListener("click",(e=>{if(e.target.matches("a")){e.preventDefault();const n=e.target.href;window.history.pushState({},"",n),t()}})),t()}();
