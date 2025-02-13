a14662fa98091d14889373411dd2a1ac89991535
Evgeniya Bashieva <eugenia.bashieva@remago.com>
2025-02-13 10:02:10 +0100

    fix: types errors

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,10 @@
-declare const PRODUCTION: boolean;
-declare const PREFIX: string;
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  VITE_PREFIX: string;  // Add any environment variables you want to access here
+  // You can add more VITE_* variables if necessary
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
